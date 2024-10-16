c7b7cfe5afedaf536c1ef30543dbab870e673358
youssefrramdan <youfahmed353@gmail.com>
2024-10-16 10:43:18 +0300

    aas

--- auth/auth.routes.js
+++ auth/auth.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { changeUserPassword, Signin, Signup } from "../auth/auth.controller.js";
+import { checkEmail } from "../middlewares/checkEmail.js";
 
 const authRouter = Router();
 authRouter.post("/signup", checkEmail,Signup);

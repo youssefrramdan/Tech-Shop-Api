2a2ed0676143f9fd5ff181950b4e93f44f2fca6f
youssefrramdan <youfahmed353@gmail.com>
2024-10-18 19:18:27 +0300

    final touchess

--- modules/bootstrap.js
+++ modules/bootstrap.js
@@ -14,7 +14,7 @@ export const bootstrap = (app) => {
   app.use("/api/brand", brandRouter);
   app.use("/api/products", productRouter);
   app.use("/api/users", userRouter);
-  app.use("/api/auth/", authRouter);
+  app.use("/api/auth", authRouter);
   app.use("/api/coupons" ,couponRouter)
   app.use("/api/carts" ,cartRouter)
   app.use("/api/orders" ,orderRouter)

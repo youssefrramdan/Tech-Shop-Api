ec612b732ca49d18dc042740a7e26e36eac432f2
youssefrramdan <youfahmed353@gmail.com>
2024-10-19 16:55:10 +0300

    help2

--- index.js
+++ index.js
@@ -4,34 +4,28 @@ import { dbConnect } from "./databases/dbConnection.js";
 import { globalError } from "./middlewares/globalError.js";
 import { AppError } from "./utils/appError.js";
 import dotenv from "dotenv";
-import cors from "cors";  // Ensure cors is imported
-import cookieParser from 'cookie-parser'; // Correctly import cookie-parser
+import cors from "cors";  
+import cookieParser from 'cookie-parser';
 
 dotenv.config({ path: "./config.env" });
 const app = express();
+app.use(express.json());
 
-// CORS configuration
 app.use(cors({
-  origin: 'http://localhost:5173',  // Allow localhost for development
-  credentials: true  // Include credentials (cookies, authorization headers, etc.) in CORS requests
+  origin: 'http://localhost:5173',
+  credentials: true  
 }));
 
-// Middleware to parse cookies
-app.use(cookieParser()); // Initialize cookie-parser
+app.use(cookieParser()); 
 
 app.use("/uploads", express.static("./uploads"));
-app.use(express.json()); // Parse incoming JSON requests
 
-// Bootstrapping the app
 bootstrap(app);
 
-// Handle unknown routes
 app.use("*", (req, res, next) => {
   next(new AppError(`Route not found: ${req.originalUrl}`, 404));
 });
 
-// Global error handling middleware
 app.use(globalError);
 
-// Start the server
 app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
--- middlewares/checkCategoryExists.js
+++ middlewares/checkCategoryExists.js
@@ -3,10 +3,9 @@ import { AppError } from "../utils/appError.js";
 
 export const checkCategoryExists = async (req, res, next) => {
   try {
-    let categoryId = req.body.category;
 
-    if (!categoryId) {
-      return next(new AppError("categoryId is required", 400));
+    if (!req.body.category) {
+      return next(new AppError("Category ID is required", 400));
     }
 
     const category = await Category.findById(categoryId);

d3fa794fc7d6cffc9964362187e64ac447dd0d05
youssefrramdan <youfahmed353@gmail.com>
2025-01-31 00:49:15 +0200

    edit route

--- auth/auth.routes.js
+++ auth/auth.routes.js
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import { changeUserPassword, Signin, Signup } from "../auth/auth.controller.js";
 import { checkEmail } from "../middlewares/checkEmail.js";
+import { Passport } from "passport";
 
 const authRouter = Router();
 authRouter.post("/signup", checkEmail,Signup);
 authRouter.post("/signin", Signin);
 authRouter.patch("/changepassword", changeUserPassword);
-authRouter.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+authRouter.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
 authRouter.get(
   "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/login" }),
+  Passport.authenticate("google", { failureRedirect: "/login" }),
   (req, res) => {
     res.redirect("/dashboard");
   }
